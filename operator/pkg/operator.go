6eae62c2caac6653756fdd6e309df23d51d3a66e
PatStiles <pdstiles78@gmail.com>
2024-10-24 18:27:00 -0300

    add fallback to operator

--- operator/pkg/operator.go
+++ operator/pkg/operator.go
@@ -548,6 +548,7 @@ func (o *Operator) verify(verificationData VerificationData, disabledVerifiersBi
 	}
 }
 
+// TODO: delete or not
 func (o *Operator) handleVerificationResult(results chan bool, isVerified bool, err error, name string) {
 	if err != nil {
 		o.Logger.Errorf("%v failed %v", name, err)
