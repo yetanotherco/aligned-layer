ad932ab3357a037f4090bf3f9542e091dae611e0
avilagaston9 <gavila@fi.uba.ar>
2024-10-28 12:17:10 -0300

    refactor: rename old_task_gas_price variable

--- batcher/aligned-batcher/src/lib.rs
+++ batcher/aligned-batcher/src/lib.rs
@@ -1221,10 +1221,10 @@ impl Batcher {
 
     // Sends a transaction to Ethereum with the same nonce as the previous one to override it.
     // We use the maximum number of retries here because this transaction must be included.
-    pub async fn cancel_create_new_task_tx(&self, old_task_gas_price: U256) {
+    pub async fn cancel_create_new_task_tx(&self, old_tx_gas_price: U256) {
         info!("Cancelling createNewTask transaction...");
         let iteration = Arc::new(Mutex::new(0));
-        let previous_gas_price = Arc::new(Mutex::new(old_task_gas_price));
+        let previous_gas_price = Arc::new(Mutex::new(old_tx_gas_price));
 
         if let Err(e) = retry_function(
             || {
