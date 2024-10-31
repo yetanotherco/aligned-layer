1d285829a1dd8c4814a0dd0fbea0bc860afc97e7
avilagaston9 <gavila@fi.uba.ar>
2024-10-31 17:37:08 -0300

    test: add tests to CI

--- batcher/aligned-batcher/src/retry/batcher_retryables.rs
+++ batcher/aligned-batcher/src/retry/batcher_retryables.rs
@@ -126,7 +126,6 @@ mod test {
         (anvil, payment_service)
     }
 
-    #[ignore]
     #[tokio::test]
     async fn test_get_user_balance_retryable() {
         let payment_service;
@@ -159,7 +158,6 @@ mod test {
         assert_eq!(balance, U256::zero());
     }
 
-    #[ignore]
     #[tokio::test]
     async fn test_user_balance_is_unlocked_retryable() {
         let payment_service;
@@ -202,7 +200,6 @@ mod test {
         assert_eq!(unlocked, false);
     }
 
-    #[ignore]
     #[tokio::test]
     async fn test_get_user_nonce_retryable() {
         let payment_service;
@@ -245,7 +242,6 @@ mod test {
         assert_eq!(nonce, U256::zero());
     }
 
-    #[ignore]
     #[tokio::test]
     async fn test_get_gas_price_retryable() {
         let eth_rpc_provider;
