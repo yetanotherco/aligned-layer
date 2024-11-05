3614696f233f023735492ba15cbdc520714eaee0
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-11-05 09:52:56 -0300

    test: eth utils CalculateGasPriceBumpBasedOnRetry

---
+++ core/utils/eth_client_utils_test.go
@@ -0,0 +1,38 @@
+package utils_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/yetanotherco/aligned_layer/core/utils"
+)
+
+func TestCalculateGasPriceBumpBasedOnRetry(t *testing.T) {
+	n := 5
+	baseBumpPercentage := 20
+	incrementalRetryPercentage := 5
+	gasPrices := [5]*big.Int{
+		big.NewInt(3000000000),
+		big.NewInt(3000000000),
+		big.NewInt(4000000000),
+		big.NewInt(4000000000),
+		big.NewInt(5000000000)}
+
+	expectedBumpedGasPrices := [5]*big.Int{
+		big.NewInt(3600000000),
+		big.NewInt(3750000000),
+		big.NewInt(5200000000),
+		big.NewInt(5400000000),
+		big.NewInt(7000000000)}
+
+	for i := range n {
+		currentGasPrice := gasPrices[i]
+		bumpedGasPrice := utils.CalculateGasPriceBumpBasedOnRetry(currentGasPrice, baseBumpPercentage, incrementalRetryPercentage, i)
+		expectedGasPrice := expectedBumpedGasPrices[i]
+
+		if bumpedGasPrice.Cmp(expectedGasPrice) != 0 {
+			t.Errorf("Bumped gas price does not match expected gas price, expected value %v, got: %v", expectedGasPrice, bumpedGasPrice)
+		}
+	}
+
+}
