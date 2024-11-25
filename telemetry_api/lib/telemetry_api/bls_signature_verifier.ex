c448a77a27bebf200d01372431ebf76ef9ea59bd
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-11-25 20:03:40 -0300

    feat: BlsSignatureVerifier module using go program

--- telemetry_api/lib/telemetry_api/bls_signature_verifier.ex
+++ telemetry_api/lib/telemetry_api/bls_signature_verifier.ex
@@ -1,6 +1,22 @@
 defmodule BlsSignatureVerifier do
-  use Rustler, otp_app: :bls_recover, crate: "blssignaturerecover"
+  def verify(signature, public_key, message) do
+    # Encode the args as hex
+    args = [
+      "--signature",
+      Base.encode16(signature, case: :lower),
+      "--publickey",
+      Base.encode16(public_key, case: :lower),
+      "--message",
+      Base.encode16(message, case: :lower)
+    ]
 
-  # Define the Rust NIF function
-  def recover_public_key(signature, message_hash), do: :erlang.nif_error(:nif_not_loaded)
+    binary_path = Path.join(:code.priv_dir(:telemetry_api), "bls_verify")
+    {output, exit_code} = System.cmd(binary_path, args)
+
+    case exit_code do
+      0 -> {:ok, "Valid"}
+      1 -> {:error, "Invalid signature"}
+      _ -> {:error, "Verification failed: #{output}"}
+    end
+  end
 end
