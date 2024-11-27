5136bf393895ea9933bd785d6c4a6297ba229209
Julian Ventura <julian.ventura@lambdaclass.com>
2024-11-27 15:47:13 -0300

    Parse error codes from ethereum contract on createNewTask

--- batcher/aligned-batcher/src/types/errors.rs
+++ batcher/aligned-batcher/src/types/errors.rs
@@ -1,7 +1,47 @@
 use std::fmt;
 
-use ethers::types::{Address, SignatureError};
+use ethers::types::{Address, Bytes, SignatureError};
 use tokio_tungstenite::tungstenite;
+
+pub enum TransactionSendError {
+    NoProofSubmitters,
+    NoFeePerProof,
+    InsufficientFeeForAggregator,
+    SubmissionInsufficientBalance,
+    BatchAlreadySubmitted,
+    InsufficientFunds,
+    OnlyBatcherAllowed,
+    Generic(String),
+}
+
+// TODO: Here we are missing the modifiers: onlyWhenNotPaused
+// TODO: Parse parameters from error responses
+
+impl From<Bytes> for TransactionSendError {
+    fn from(e: Bytes) -> Self {
+        let byte_string = e.to_string();
+        let str_code = if byte_string.len() >= 10 {
+            &byte_string[..10] // Extract the error code only
+        } else {
+            "" // Not gonna match
+        };
+        match str_code {
+            "0xc43ac290" => TransactionSendError::NoProofSubmitters, // can't happen
+            "0xa3a8658a" => TransactionSendError::NoFeePerProof,     // can't happen
+            "0x7899ec71" => TransactionSendError::InsufficientFeeForAggregator, // shouldn't happen,
+            // returning the proofs and retrying later may help
+            "0x4f779ceb" => TransactionSendError::SubmissionInsufficientBalance, // shouldn't happen,
+            // flush can help if something went wrong
+            "0x3102f10c" => TransactionSendError::BatchAlreadySubmitted,
+            "0x5c54305e" => TransactionSendError::InsufficientFunds,
+            "0x152bc288" => TransactionSendError::OnlyBatcherAllowed,
+            // flush can help if something went wrong
+            _ => {
+                TransactionSendError::Generic(format!("Unknown bytestring error: {}", byte_string))
+            }
+        }
+    }
+}
 
 pub enum BatcherError {
     TcpListenerError(String),
@@ -12,7 +52,7 @@ pub enum BatcherError {
     BatchUploadError(String),
     TaskCreationError(String),
     ReceiptNotFoundError,
-    TransactionSendError(String),
+    TransactionSendError(TransactionSendError),
     MaxRetriesReachedError,
     SerializationError(String),
     GasPriceError,
@@ -97,6 +137,37 @@ impl fmt::Debug for BatcherError {
                     "Error while trying to get disabled verifiers: {}",
                     reason
                 )
+            }
+        }
+    }
+}
+
+impl fmt::Display for TransactionSendError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TransactionSendError::NoProofSubmitters => {
+                write!(f, "No proof submitter error")
+            }
+            TransactionSendError::NoFeePerProof => {
+                write!(f, "No fee per proof")
+            }
+            TransactionSendError::InsufficientFeeForAggregator => {
+                write!(f, "Insufficient fee for aggregator")
+            }
+            TransactionSendError::SubmissionInsufficientBalance => {
+                write!(f, "Submission insufficient balance")
+            }
+            TransactionSendError::BatchAlreadySubmitted => {
+                write!(f, "Batch already submitted")
+            }
+            TransactionSendError::InsufficientFunds => {
+                write!(f, "Insufficient funds")
+            }
+            TransactionSendError::OnlyBatcherAllowed => {
+                write!(f, "Only batcher allowed")
+            }
+            TransactionSendError::Generic(e) => {
+                write!(f, "Generic error: {}", e)
             }
         }
     }
