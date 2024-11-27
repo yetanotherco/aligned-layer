116d3f6ead7755377e7c335d3fab6ba8b0ad424a
Julian Ventura <julian.ventura@lambdaclass.com>
2024-11-27 17:52:29 -0300

    Add simulate_create_new_task

--- batcher/aligned-batcher/src/retry/batcher_retryables.rs
+++ batcher/aligned-batcher/src/retry/batcher_retryables.rs
@@ -175,6 +175,65 @@ pub async fn create_new_task_retryable(
         .ok_or(RetryError::Permanent(BatcherError::ReceiptNotFoundError))
 }
 
+pub async fn simulate_create_new_task_retryable(
+    batch_merkle_root: [u8; 32],
+    batch_data_pointer: String,
+    proofs_submitters: Vec<Address>,
+    fee_params: CreateNewTaskFeeParams,
+    payment_service: &BatcherPaymentService,
+    payment_service_fallback: &BatcherPaymentService,
+) -> Result<(), RetryError<BatcherError>> {
+    info!("Simulating task for: 0x{}", hex::encode(batch_merkle_root));
+    let simulation_fallback;
+    let simulation = payment_service
+        .create_new_task(
+            batch_merkle_root,
+            batch_data_pointer.clone(),
+            proofs_submitters.clone(),
+            fee_params.fee_for_aggregator,
+            fee_params.fee_per_proof,
+            fee_params.respond_to_task_fee_limit,
+        )
+        .gas_price(fee_params.gas_price);
+
+    match simulation.call().await {
+        Ok(_) => Ok(()),
+        Err(ContractError::Revert(err)) => {
+            // Since transaction was reverted, we don't want to retry with fallback.
+            warn!("Simulated transaction reverted {:?}", err);
+            return Err(RetryError::Permanent(BatcherError::TransactionSendError(
+                err.to_string(),
+            )));
+        }
+        _ => {
+            simulation_fallback = payment_service_fallback
+                .create_new_task(
+                    batch_merkle_root,
+                    batch_data_pointer,
+                    proofs_submitters,
+                    fee_params.fee_for_aggregator,
+                    fee_params.fee_per_proof,
+                    fee_params.respond_to_task_fee_limit,
+                )
+                .gas_price(fee_params.gas_price);
+            match simulation_fallback.call().await {
+                Ok(_) => Ok(()),
+                Err(ContractError::Revert(err)) => {
+                    warn!("Simulated transaction reverted {:?}", err);
+                    return Err(RetryError::Permanent(BatcherError::TransactionSendError(
+                        err.to_string(),
+                    )));
+                }
+                Err(err) => {
+                    return Err(RetryError::Transient(BatcherError::TransactionSendError(
+                        err.to_string(),
+                    )))
+                }
+            }
+        }
+    }
+}
+
 pub async fn cancel_create_new_task_retryable(
     batcher_signer: &SignerMiddlewareT,
     batcher_signer_fallback: &SignerMiddlewareT,
