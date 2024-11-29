c22033ad49a07d3021f28d47893454e3b579a231
avilagaston9 <gavila@fi.uba.ar>
2024-11-29 12:16:50 -0300

    trigger CI

--- batcher/aligned-batcher/src/lib.rs
+++ batcher/aligned-batcher/src/lib.rs
@@ -1124,7 +1124,7 @@ impl Batcher {
         let mut batch_state_lock = self.batch_state.lock().await;
         let current_batch_len = batch_state_lock.batch_queue.len();
         let last_uploaded_batch_block_lock = self.last_uploaded_batch_block.lock().await;
-        let min_len = 1;
+        let min_len = 2;
 
         if current_batch_len < min_len {
             info!(
