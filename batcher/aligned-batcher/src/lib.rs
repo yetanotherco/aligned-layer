d110ab35045102865d6119004331a4a1fc00a062
avilagaston9 <gavila@fi.uba.ar>
2024-11-29 12:31:48 -0300

    fix: restore changes

--- batcher/aligned-batcher/src/lib.rs
+++ batcher/aligned-batcher/src/lib.rs
@@ -1124,9 +1124,8 @@ impl Batcher {
         let mut batch_state_lock = self.batch_state.lock().await;
         let current_batch_len = batch_state_lock.batch_queue.len();
         let last_uploaded_batch_block_lock = self.last_uploaded_batch_block.lock().await;
-        let min_len = 2;
 
-        if current_batch_len < min_len {
+        if current_batch_len < 1 {
             info!(
                 "Current batch has {} proofs. Waiting for more proofs...",
                 current_batch_len
