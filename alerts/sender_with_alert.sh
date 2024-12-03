81a5fe226c14435b96a629ea24e4b5ad0c96da43
JuArce <52429267+JuArce@users.noreply.github.com>
2024-12-03 11:53:42 -0300

    feat: add infinite loop

--- alerts/sender_with_alert.sh
+++ alerts/sender_with_alert.sh
@@ -26,55 +26,62 @@ function send_pagerduty_alert() {
 
 #################
 
-## Remove Proof Data
-rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
-rm -rf ./aligned_verification_data/*
+while true
+do
 
-mkdir -p ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs
+  ## Remove Proof Data
+  rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
+  rm -rf ./aligned_verification_data/*
 
-## Generate Proof
-x=1
-echo "Generating proof $x != 0"
-go run ./scripts/test_files/gnark_groth16_bn254_infinite_script/cmd/main.go $x
+  mkdir -p ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs
 
-## Send Proof
-submit=$(aligned submit \
-	--proving_system Groth16Bn254 \
-	--repetitions $REPETITIONS \
-	--proof "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.proof" \
-	--public_input "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.pub" \
-	--vk "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.vk" \
-	--proof_generator_addr $SENDER_ADDRESS \
-	--keystore_path $KEYSTORE_PATH \
-	--rpc_url $RPC_URL \
-	--batcher_url $BATCHER_URL \
-	--network $NETWORK \
-	2>&1)
+  ## Generate Proof
+  x=1
+  echo "Generating proof $x != 0"
+  go run ./scripts/test_files/gnark_groth16_bn254_infinite_script/cmd/main.go $x
 
-echo "$submit"
-
-explorer_link=$(echo "$submit" | grep alignedlayer.com | awk '{print $4}')
-sleep 60
-
-for proof in ./aligned_verification_data/*.cbor; do
-  ## Validate Proof on Chain
-  verification=$(aligned verify-proof-onchain \
-    --aligned-verification-data $proof \
+  ## Send Proof
+  submit=$(aligned submit \
+    --proving_system Groth16Bn254 \
+    --repetitions $REPETITIONS \
+    --proof "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.proof" \
+    --public_input "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.pub" \
+    --vk "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.vk" \
+    --proof_generator_addr $SENDER_ADDRESS \
+    --keystore_path $KEYSTORE_PATH \
     --rpc_url $RPC_URL \
+    --batcher_url $BATCHER_URL \
     --network $NETWORK \
     2>&1)
 
-  ## Send Alert is Verification Fails
-  if echo "$verification" | grep -q not; then
-    message="Proof verification failed for $proof [ $explorer_link ]"
-    echo "$message"
-    #send_pagerduty_alert "$message"
-    break
-  elif echo "$verification" | grep -q verified; then
-    echo "Proof verification succeeded for $proof"
-  fi
-done
+  echo "$submit"
 
-## Remove Proof Data
-rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
-rm -rf ./aligned_verification_data/*
+  explorer_link=$(echo "$submit" | grep alignedlayer.com | awk '{print $4}')
+  sleep 60
+
+  for proof in ./aligned_verification_data/*.cbor; do
+    ## Validate Proof on Chain
+    verification=$(aligned verify-proof-onchain \
+      --aligned-verification-data $proof \
+      --rpc_url $RPC_URL \
+      --network $NETWORK \
+      2>&1)
+
+    ## Send Alert is Verification Fails
+    if echo "$verification" | grep -q not; then
+      message="Proof verification failed for $proof [ $explorer_link ]"
+      echo "$message"
+      #send_pagerduty_alert "$message"
+      break
+    elif echo "$verification" | grep -q verified; then
+      echo "Proof verification succeeded for $proof"
+    fi
+  done
+
+  ## Remove Proof Data
+  rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
+  rm -rf ./aligned_verification_data/*
+
+  sleep $SLEEP
+  counter=$((counter + 1))
+done
