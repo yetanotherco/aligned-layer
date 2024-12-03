4b0d673d47d6ad4effd141718a6c0081003203ef
JuArce <52429267+JuArce@users.noreply.github.com>
2024-12-02 21:58:26 -0300

    fix: verify all sent proofs fix: paths feat: add repetitions as parameter

--- alerts/sender_with_alert.sh
+++ alerts/sender_with_alert.sh
@@ -27,20 +27,20 @@ function send_pagerduty_alert() {
 #################
 
 ## Remove Proof Data
-rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
-rm -rf ./aligned_verification_data/*
+rm -rf ../scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
+rm -rf ../aligned_verification_data/*
 
-mkdir -p scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs
+mkdir -p ../scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs
 
 ## Generate Proof
 x=1
 echo "Generating proof $x != 0"
-go run scripts/test_files/gnark_groth16_bn254_infinite_script/cmd/main.go $x
+go run ../scripts/test_files/gnark_groth16_bn254_infinite_script/cmd/main.go $x
 
 ## Send Proof
 submit=$(aligned submit \
 	--proving_system Groth16Bn254 \
-	--repetitions 32 \
+	--repetitions $REPETITIONS \
 	--proof "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.proof" \
 	--public_input "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.pub" \
 	--vk "./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/ineq_${x}_groth16.vk" \
@@ -54,24 +54,27 @@ submit=$(aligned submit \
 echo "$submit"
 
 explorer_link=$(echo "$submit" | grep alignedlayer.com | awk '{print $4}')
-sleep 1
+sleep 60
 
+for proof in ./aligned_verification_data/*.cbor; do
 ## Validate Proof on Chain
-verification=$(aligned verify-proof-onchain \
-	--aligned-verification-data ./aligned_verification_data/*.cbor \
-	--rpc_url $RPC_URL \
-       	--network $NETWORK \
-	2>&1);
+  verification=$(aligned verify-proof-onchain \
+    --aligned-verification-data ./aligned_verification_data/*.cbor \
+    --rpc_url $RPC_URL \
+    --network $NETWORK \
+    2>&1)
 
-## Send Alert is Verification Fails
-if echo "$verification" | grep -q not; then
-	message="Proof verification failed for: $explorer_link";
-	echo "$message"
-	send_pagerduty_alert "$message"
-elif echo "$verification" | grep -q verified; then
-	echo echo "Proof verification succeeded for $explorer_link";
-fi
+  ## Send Alert is Verification Fails
+  if echo "$verification" | grep -q not; then
+    message="Proof verification failed for $proof [ $explorer_link ]"
+    echo "$message"
+    send_pagerduty_alert "$message"
+    break
+  elif echo "$verification" | grep -q verified; then
+    echo echo "Proof verification succeeded for $proof"
+  fi
+done
 
 ## Remove Proof Data
-rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
-rm -rf ./aligned_verification_data/*
+rm -rf ../scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
+rm -rf ../aligned_verification_data/*
