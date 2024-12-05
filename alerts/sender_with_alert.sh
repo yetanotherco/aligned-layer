32edd3cd091be4b850c77cb233ed82e7dcf3d076
JuArce <52429267+JuArce@users.noreply.github.com>
2024-12-05 14:26:45 -0300

    feat: send update to slack feat: use nonce to generate proof

--- alerts/sender_with_alert.sh
+++ alerts/sender_with_alert.sh
@@ -23,12 +23,16 @@ source "$1"
 # Function to send PagerDuty alert
 # @param message
 function send_pagerduty_alert() {
-  . pagerduty.sh "$1"
+  . alerts/pagerduty.sh "$1"
+}
+
+# Function so send Slack message
+# @param message
+function send_slack_message() {
+  . alerts/slack.sh "$1"
 }
 
 #################
-x=100
-
 while true
 do
 
@@ -39,8 +43,13 @@ do
   mkdir -p ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs
 
   ## Generate Proof
+  x=$(aligned get-user-nonce --batcher_url $BATCHER_URL --user_addr $SENDER_ADDRESS 2>&1 | awk '{print $9}')
   echo "Generating proof $x != 0"
   go run ./scripts/test_files/gnark_groth16_bn254_infinite_script/cmd/main.go $x
+
+  ## Get initial balance
+  initial_balance=$(aligned get-user-balance --rpc_url $RPC_URL --user_addr  $SENDER_ADDRESS --network $NETWORK 2>&1 | awk '{print $7}')
+  echo "Initial balance: $initial_balance"
 
   ## Send Proof
   echo "Submitting $REPETITIONS proofs $x != 0"
@@ -63,6 +72,14 @@ do
   explorer_link=$(echo "$submit" | grep alignedlayer.com | awk '{print $4}')
   sleep 600
 
+  ## Get final balance
+  final_balance=$(aligned get-user-balance --rpc_url $RPC_URL --user_addr  $SENDER_ADDRESS --network $NETWORK 2>&1 | awk '{print $7}')
+  echo "Final balance: $final_balance"
+
+  ## Calculate the spent amount
+  spent_amount=$(echo "$initial_balance - $final_balance" | bc | awk '{printf "%.9f", $1}')
+
+  ## Verify Proofs
   echo "Verifying $REPETITIONS proofs $x != 0"
   for proof in ./aligned_verification_data/*.cbor; do
     ## Validate Proof on Chain
@@ -83,11 +100,16 @@ do
     fi
   done
 
+  ## Send Update to Slack
+  eth_usd=$(curl https://cryptoprices.cc/ETH/)
+  spent_ammount_usd=$(echo "$spent_amount * $eth_usd" | bc | awk '{printf "%.2f", $1}')
+  slack_meesage="$REPETITIONS Proofs submitted and verified. Spent amount: $spent_amount ETH ($ $spent_ammount_usd) [ $explorer_link ]"
+  send_slack_message "$slack_meesage"
+
   ## Remove Proof Data
   rm -rf ./scripts/test_files/gnark_groth16_bn254_infinite_script/infinite_proofs/*
   rm -rf ./aligned_verification_data/*
 
   echo "Sleeping $SLEEP seconds"
   sleep $SLEEP
-  x=$((x + 1))
 done
