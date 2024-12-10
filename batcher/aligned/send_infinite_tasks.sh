c869a865ca3d3b9ef43362b4369aaba99621b631
Mario Rugiero <mrugiero@gmail.com>
2024-12-10 14:42:44 -0300

    update senders to use wss

--- batcher/aligned/send_infinite_tasks.sh
+++ batcher/aligned/send_infinite_tasks.sh
@@ -14,7 +14,7 @@ fi
 
 # Set default values for RPC and BATCHER if they are not set
 RPC=${RPC:-http://localhost:8545}
-BATCHER_CONN=${BATCHER_CONN:-ws://localhost:8080}
+BATCHER_CONN=${BATCHER_CONN:-wss://localhost:8080}
 if [ -z "$NETWORK" ]; then
     echo "NETWORK is not set. Setting it to devnet"
     NETWORK="devnet"
