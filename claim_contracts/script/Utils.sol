178c522d60e51695108409e1a8816be38a4af9d0
ilitteri <ilitteri@fi.uba.ar>
2024-12-12 19:12:59 -0300

    Add utils

--- claim_contracts/script/Utils.sol
+++ claim_contracts/script/Utils.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.13;
 
 import {Vm} from "forge-std/Vm.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "../src/AlignedTokenV1.sol";
+import "../src/AlignedTokenV2Example.sol";
+import "../src/ClaimableAirdropV1.sol";
+import "../src/ClaimableAirdropV2Example.sol";
 
 library Utils {
     // Cheatcodes address, 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D.
@@ -69,4 +74,240 @@ library Utils {
             addr := mload(add(addressOffset, 20))
         }
     }
+
+    function deployAlignedTokenImplementation(
+        uint256 _version
+    ) internal returns (address) {
+        address _implementation_address;
+        if (_version == 1) {
+            vm.broadcast();
+            AlignedTokenV1 _implementation = new AlignedTokenV1();
+            _implementation_address = address(_implementation);
+        } else if (_version == 2) {
+            vm.broadcast();
+            AlignedTokenV2Example _implementation = new AlignedTokenV2Example();
+            _implementation_address = address(_implementation);
+        } else {
+            revert("Unsupported version");
+        }
+        return _implementation_address;
+    }
+
+    function alignedTokenProxyDeploymentData(
+        address _implementation,
+        uint256 _version,
+        address _safe,
+        address _beneficiary1,
+        address _beneficiary2,
+        address _beneficiary3,
+        uint256 _mintAmount
+    ) internal pure returns (bytes memory) {
+        return
+            abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                alignedTokenInitData(
+                    _implementation,
+                    _version,
+                    _safe,
+                    _beneficiary1,
+                    _beneficiary2,
+                    _beneficiary3,
+                    _mintAmount
+                )
+            );
+    }
+
+    function alignedTokenInitData(
+        address _implementation,
+        uint256 _version,
+        address _safe,
+        address _beneficiary1,
+        address _beneficiary2,
+        address _beneficiary3,
+        uint256 _mintAmount
+    ) internal pure returns (bytes memory) {
+        if (_version == 1) {
+            return
+                abi.encodeCall(
+                    AlignedTokenV1(_implementation).initialize,
+                    (
+                        _safe,
+                        _beneficiary1,
+                        _mintAmount,
+                        _beneficiary2,
+                        _mintAmount,
+                        _beneficiary3,
+                        _mintAmount
+                    )
+                );
+        } else if (_version == 2) {
+            return
+                abi.encodeCall(
+                    AlignedTokenV2Example(_implementation).initialize,
+                    (
+                        _safe,
+                        _beneficiary1,
+                        _mintAmount,
+                        _beneficiary2,
+                        _mintAmount,
+                        _beneficiary3,
+                        _mintAmount
+                    )
+                );
+        } else {
+            revert("Unsupported version");
+        }
+    }
+
+    function alignedTokenUpgradeData(
+        address _newImplementation,
+        uint256 _version,
+        address _safe,
+        address _beneficiary1,
+        address _beneficiary2,
+        address _beneficiary3,
+        uint256 _mintAmount
+    ) internal pure returns (bytes memory) {
+        bytes memory _initData = Utils.alignedTokenInitData(
+            _newImplementation,
+            _version,
+            _safe,
+            _beneficiary1,
+            _beneficiary2,
+            _beneficiary3,
+            _mintAmount
+        );
+        bytes memory _upgradeData;
+        if (_version == 1) {
+            _upgradeData = abi.encodeCall(
+                AlignedTokenV1(_newImplementation).upgradeToAndCall,
+                (_newImplementation, _initData)
+            );
+        } else if (_version == 2) {
+            _upgradeData = abi.encodeCall(
+                AlignedTokenV2Example(_newImplementation).upgradeToAndCall,
+                (_newImplementation, _initData)
+            );
+        } else {
+            revert("Unsupported version");
+        }
+        return _upgradeData;
+    }
+
+    function deployClaimableAirdropImplementation(
+        uint256 _version
+    ) internal returns (address) {
+        address _implementation_address;
+        if (_version == 1) {
+            vm.broadcast();
+            ClaimableAirdropV1 _implementation = new ClaimableAirdropV1();
+            _implementation_address = address(_implementation);
+        } else if (_version == 2) {
+            vm.broadcast();
+            ClaimableAirdropV2Example _implementation = new ClaimableAirdropV2Example();
+            _implementation_address = address(_implementation);
+        } else {
+            revert("Unsupported version");
+        }
+        return _implementation_address;
+    }
+
+    function claimableAirdropProxyDeploymentData(
+        address _implementation,
+        uint256 _version,
+        address _safe,
+        address _tokenContractAddress,
+        address _tokenOwnerAddress,
+        uint256 _limitTimestampToClaim,
+        bytes32 _claimMerkleRoot
+    ) internal pure returns (bytes memory) {
+        return
+            abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                claimableAirdropInitData(
+                    _implementation,
+                    _version,
+                    _safe,
+                    _tokenContractAddress,
+                    _tokenOwnerAddress,
+                    _limitTimestampToClaim,
+                    _claimMerkleRoot
+                )
+            );
+    }
+
+    function claimableAirdropInitData(
+        address _implementation,
+        uint256 _version,
+        address _safe,
+        address _tokenContractAddress,
+        address _tokenOwnerAddress,
+        uint256 _limitTimestampToClaim,
+        bytes32 _claimMerkleRoot
+    ) internal pure returns (bytes memory) {
+        if (_version == 1) {
+            return
+                abi.encodeCall(
+                    ClaimableAirdropV1(_implementation).initialize,
+                    (
+                        _safe,
+                        _tokenContractAddress,
+                        _tokenOwnerAddress,
+                        _limitTimestampToClaim,
+                        _claimMerkleRoot
+                    )
+                );
+        } else if (_version == 2) {
+            return
+                abi.encodeCall(
+                    ClaimableAirdropV2Example(_implementation).initialize,
+                    (
+                        _safe,
+                        _tokenContractAddress,
+                        _tokenOwnerAddress,
+                        _limitTimestampToClaim,
+                        _claimMerkleRoot
+                    )
+                );
+        } else {
+            revert("Unsupported version");
+        }
+    }
+
+    function claimableAirdropUpgradeData(
+        address _newImplementation,
+        uint256 _version,
+        address _safe,
+        address _tokenContractAddress,
+        address _tokenOwnerAddress,
+        uint256 _limitTimestampToClaim,
+        bytes32 _claimMerkleRoot
+    ) internal pure returns (bytes memory) {
+        bytes memory _initData = Utils.claimableAirdropInitData(
+            _newImplementation,
+            _version,
+            _safe,
+            _tokenContractAddress,
+            _tokenOwnerAddress,
+            _limitTimestampToClaim,
+            _claimMerkleRoot
+        );
+        bytes memory _upgradeData;
+        if (_version == 1) {
+            _upgradeData = abi.encodeCall(
+                ClaimableAirdropV1(_newImplementation).upgradeToAndCall,
+                (_newImplementation, _initData)
+            );
+        } else if (_version == 2) {
+            _upgradeData = abi.encodeCall(
+                ClaimableAirdropV2Example(_newImplementation).upgradeToAndCall,
+                (_newImplementation, _initData)
+            );
+        } else {
+            revert("Unsupported version");
+        }
+        return _upgradeData;
+    }
+
+    
 }
