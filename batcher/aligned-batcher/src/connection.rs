7e660ade6262d55dad1e4be14a80e68a8b5df25e
Uriel Mihura <43704209+uri-99@users.noreply.github.com>
2024-12-18 13:24:41 -0300

    Update batcher/aligned-batcher/src/connection.rs

--- batcher/aligned-batcher/src/connection.rs
+++ batcher/aligned-batcher/src/connection.rs
@@ -22,7 +22,7 @@ pub(crate) async fn send_batch_inclusion_data_responses(
     batch_merkle_tree: &MerkleTree<VerificationCommitmentBatch>,
 ) -> Result<(), BatcherError> {
     // iter in reverse because each sender wants to receive responses in ascending nonce order
-    // and finalized_batch is ordered as the PriorityQueue , low max_nonce first && high nonce first.
+    // and finalized_batch is ordered as the PriorityQueue , low max_fee first && high nonce first.
     for (vd_batch_idx, entry) in finalized_batch.iter().enumerate().rev() {
         let batch_inclusion_data = BatchInclusionData::new(
             vd_batch_idx,
