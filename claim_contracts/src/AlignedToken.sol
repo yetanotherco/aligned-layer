cf4e40a9b15ca6da5309f527f4bfe857033f54e5
ilitteri <ilitteri@fi.uba.ar>
2024-12-18 00:54:57 -0300

    Last ERC20 fixes

--- claim_contracts/src/AlignedToken.sol
+++ claim_contracts/src/AlignedToken.sol
@@ -8,6 +8,7 @@ import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/toke
 import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 /// @title Aligned Token
+/// @custom:security-contact If you find a security bug, please contact us at security@alignedfoundation.org
 /// @notice This contract is the implementation of the Aligned Token
 /// @dev This contract is upgradeable and should be used only through the proxy contract
 contract AlignedToken is
@@ -23,11 +24,16 @@ contract AlignedToken is
     /// @notice Symbol of the token.
     string public constant SYMBOL = "ALIGN";
 
-    /// @notice Supply of the token for the foundation.
-    uint256 public constant FOUNDATION_SUPPLY = 7_400_000_000e18; // 7.4 billion
+    /// @notice Supply of tokens for the foundation.
+    uint256 public constant INITIAL_FOUNDATION_SUPPLY = 7_400_000_000e18; // 7.4 billion
 
-    /// @notice Supply of the token for the token distributor.
-    uint256 public constant TOKEN_DISTRIBUTOR_SUPPLY = 2_600_000_000e18; // 2.6 billion
+    /// @notice Supply of tokens for the token distributor.
+    uint256 public constant INITIAL_TOKEN_DISTRIBUTOR_SUPPLY = 2_600_000_000e18; // 2.6 billion
+
+    /// @notice Event emitted when tokens are minted.
+    /// @param to address to which the tokens are minted.
+    /// @param amount amount of tokens minted.
+    event TokensMinted(address indexed to, uint256 amount);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -51,13 +57,16 @@ contract AlignedToken is
         __ERC20Permit_init(NAME);
         __ERC20Burnable_init();
         __Ownable_init(_foundation);
-        _mint(_foundation, FOUNDATION_SUPPLY);
-        _mint(_tokenDistributor, TOKEN_DISTRIBUTOR_SUPPLY);
+        _mint(_foundation, INITIAL_FOUNDATION_SUPPLY);
+        _mint(_tokenDistributor, INITIAL_TOKEN_DISTRIBUTOR_SUPPLY);
     }
 
     /// @notice Mints `amount` of tokens.
+    /// @param to address to which the tokens will be minted.
+    /// @param amount amount of tokens to mint.
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+        emit TokensMinted(to, amount);
     }
 
     /// @notice Prevents the owner from renouncing ownership.
