054d1ddec07dfa6a53192e4466485f5dc5393f01
Uriel Mihura <43704209+uri-99@users.noreply.github.com>
2025-01-22 16:07:38 -0300

    fix: remove required=true from privatekeytype in CLI (#1767)

--- batcher/aligned/src/main.rs
+++ batcher/aligned/src/main.rs
@@ -281,7 +281,7 @@ pub struct GetUserAmountOfQueuedProofsArgs {
 }
 
 #[derive(Args, Debug)]
-#[group(required = true, multiple = false)]
+#[group(multiple = false)]
 pub struct PrivateKeyType {
     #[arg(name = "path_to_keystore", long = "keystore_path")]
     keystore_path: Option<PathBuf>,
